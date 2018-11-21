0259f585d8665ce25f0a6aae51ef0e259e5378b7
atomicfruitcake <sam@wirewax.com>
2018-11-21 13:36:36 +0000

    Initial commit

---
+++ ballet/gui.py
@@ -0,0 +1,70 @@
+"""!
+@author atomicfruitcake
+
+@date 2018
+"""
+
+from Tkinter import *
+from orchestrator import Orchestrator
+
+
+class BalletApp:
+    def __init__(self, master):
+        """
+        Constructor for the GUI app
+        @param master: main window of applicaiton
+        """
+        self.master = master
+        master.title("Ballet SSH Orcherstrator")
+
+        self.label = Label(master, text="Welcome to Ballet").pack()
+
+        self.username_label = Label(master, text="Enter Username").pack()
+        self.username = StringVar()
+        self.username_field = Entry(master, textvariable=self.username).pack()
+
+        self.hosts_label = Label(master, text="Enter Hosts").pack()
+        self.hosts = StringVar()
+        self.hosts_field = Entry(master, textvariable=self.hosts).pack()
+
+        self.password_enabled = False
+        self.password_button = Button(master, text="Password required?", command=self.enable_password).pack()
+        self.password = StringVar()
+        self.password_field = Entry(master, textvariable=self.password)
+        self.password_field.config(state=DISABLED)
+        self.password_field.pack()
+
+        self.disable_known_hosts_button = Checkbutton(master, text="Disable Known Hosts?").pack()
+
+        self.run = Button(master, text="Start", command=self.check_ready_to_start).pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit).pack()
+
+    def enable_password(self):
+        """
+        Set password field to an ctive state
+        """
+        if self.password_enabled is False:
+            self.password_button.config(state=NORMAL)
+        else:
+            self.password_button.config(state=DISABLED)
+            self.password_enabled = True
+
+    def check_ready_to_start(self):
+        """
+        Check that all required fields are filled
+        """
+        print(self.username)
+        print(self.password_field.grab_status())
+
+
+def setup_root():
+    root = Tk()
+    root.geometry("600x400")
+    return root
+
+
+if __name__ == "__main__":
+    root = setup_root()
+    my_gui = BalletApp(root)
+    root.mainloop()
---
+++ ballet/orchestrator.py
@@ -0,0 +1,69 @@
+"""!
+@author atomicfruitcake
+
+@date 2018
+
+
+"""
+import multiprocessing
+from fabric.api import settings
+
+
+
+class Orchestrator:
+
+    def __init__(
+        self, keyfile, username, domains, disable_known_hosts=True, password=None
+    ):
+        """
+        Constructor for the multi SSH login
+        @param keyfile: str - path to SSH key required to access the domains
+        @param username: str - username to ssh login with
+        @param domains: list - list of domains to login to
+        @param disable_known_hosts: bool
+        @param password: str - password to use if challenged on login
+        """
+        self.keyfile = keyfile
+        self.username = username
+        self.domains = domains
+        self.disable_known_hosts = disable_known_hosts
+        self.password = password
+        self.connections = self.__establish_connections()
+
+
+    def __establish_connections(self):
+        """
+        Establish fabric SSH connections to all of the domains
+        @return: list - fabric ssh connection
+        """
+        return [
+            settings(
+                host_string="{}@{}".format(self.username, domain),
+                disable_known_hosts=self.disable_known_hosts,
+                key_filename=self.keyfile,
+                password=self.password,
+            )
+            for domain in self.domains
+        ]
+
+    def mp_connections(self):
+        """
+
+        @return:
+        """
+        return [
+            multiprocessing.Process(
+                target=connection,
+                args=()
+            )
+            for connection in self.connections
+        ]
+
+
+    def run_command(self, command):
+        """
+        Run command on all connections
+        @param command: str - command to run over all connections
+        """
+        for connection in self.connections:
+            print(connection.run(command))
